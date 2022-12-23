e6e63dfd67ef3809385a2bffb95dc1fc5e875300
Daniel Her <danieljhher@gmail.com>
2022-12-23 12:16:26 -0800

    basic template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import "./styles/styles.css";
 
-// const input = document.querySelector("#input");
-
-// const createList = () => {};
+// add event listeners for click -> open form -> event listener for when form is submitted -> take info and render into dom
 
 console.log("working");
