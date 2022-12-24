6aba273f4f449dac5f55e86657e683db1c9d96e6
Daniel Her <danieljhher@gmail.com>
2022-12-24 12:54:40 -0800

    added ui

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import "./styles/styles.css";
 
 // add event listeners for click -> open form -> event listener for when form is submitted -> take info and render into dom
 
-console.log("working");
+console.log("is it");
