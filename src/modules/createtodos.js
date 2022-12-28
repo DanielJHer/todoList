e0035d11619b94cd71e9cd9201a7cbd8192316c0
Daniel Her <danieljhher@gmail.com>
2022-12-28 12:22:13 -0800

    added logic

--- src/modules/createtodos.js
+++ src/modules/createtodos.js
@@ -1,13 +1,35 @@
-const addTask = () => {
-  console.log("working");
+const createTask = (content) => {
+  const newTask = document.createElement("div");
+  newTask.innerText = content;
 
-  // hides itself and then renders a form
-  // that form takes input and then creates object which appends into DOM
+  const taskHeader = document.querySelector(".taskHeader");
+  taskHeader.appendChild(newTask);
 };
 
 const createToDos = () => {
   const addTaskDiv = document.querySelector(".addTaskDiv");
-  addTaskDiv.addEventListener("click", addTask);
+  const inputForm = document.querySelector(".inputForm");
+  const inputText = document.querySelector(".inputText");
+
+  addTaskDiv.addEventListener("click", (e) => {
+    addTaskDiv.classList.add("hide");
+    inputForm.classList.remove("hide");
+  });
+
+  const submitBtn = document.querySelector(".submitBtn");
+  submitBtn.addEventListener("click", (e) => {
+    const inputContent = inputText.value;
+    inputText.value = "";
+    createTask(inputContent);
+    inputForm.classList.add("hide");
+    addTaskDiv.classList.remove("hide");
+  });
+
+  const cancelBtn = document.querySelector(".cancelBtn");
+  cancelBtn.addEventListener("click", (e) => {
+    inputForm.classList.add("hide");
+    addTaskDiv.classList.remove("hide");
+  });
 };
 
 export default createToDos;
