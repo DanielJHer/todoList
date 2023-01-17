9a7dc917414f29c4d04ce824a1e6458d38d9108f
Daniel Her <danieljhher@gmail.com>
2023-01-17 13:09:21 -0800

    added prettier

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,16 +1,16 @@
 const renderHomePage = () => {
   // Selecting DOM elements
-  const addTaskDiv = document.querySelector(".addTaskDiv");
-  const inputForm = document.querySelector(".inputForm");
-  const inputText = document.querySelector(".inputText");
-  const submitBtn = document.querySelector(".submitBtn");
-  const cancelBtn = document.querySelector(".cancelBtn");
-  const textArea = document.querySelector(".textArea");
-  const inputDate = document.querySelector(".inputDate");
-  const priorityList = document.getElementsByName("priority");
-  const addProjectDiv = document.querySelector(".addProjectDiv");
-  const taskListDiv = document.querySelector(".taskListDiv");
-  const projectListDiv = document.querySelector(".projectListDiv");
+  const addTaskDiv = document.querySelector('.addTaskDiv');
+  const inputForm = document.querySelector('.inputForm');
+  const inputText = document.querySelector('.inputText');
+  const submitBtn = document.querySelector('.submitBtn');
+  const cancelBtn = document.querySelector('.cancelBtn');
+  const textArea = document.querySelector('.textArea');
+  const inputDate = document.querySelector('.inputDate');
+  const priorityList = document.getElementsByName('priority');
+  const addProjectDiv = document.querySelector('.addProjectDiv');
+  const taskListDiv = document.querySelector('.taskListDiv');
+  const projectListDiv = document.querySelector('.projectListDiv');
 
   // Render Projects
   // append default project to DOM upon first render
@@ -19,19 +19,17 @@ const renderHomePage = () => {
 
   // toggle forms
   const toggleHide = () => {
-    addTaskDiv.classList.toggle("hide");
-    inputForm.classList.toggle("hide");
+    addTaskDiv.classList.toggle('hide');
+    inputForm.classList.toggle('hide');
   };
 
   // creating tasks with factory functions
-  const createTaskObject = (title, description, dueDate, priority) => {
-    return {
-      title,
-      description,
-      dueDate,
-      priority,
-    };
-  };
+  const createTaskObject = (title, description, dueDate, priority) => ({
+    title,
+    description,
+    dueDate,
+    priority,
+  });
 
   // finding priority
   const findPriority = (nodeList) => {
@@ -55,7 +53,7 @@ const renderHomePage = () => {
       dueDate.length === 0 ||
       priority.length === 0
     ) {
-      alert("Please complete all fields");
+      alert('Please complete all fields');
       return;
     }
 
@@ -68,19 +66,19 @@ const renderHomePage = () => {
     );
 
     // locally stores objects
-    const taskList = JSON.parse(localStorage.getItem("taskList") || "[]");
+    const taskList = JSON.parse(localStorage.getItem('taskList') || '[]');
     taskList.push(newTaskObject);
-    localStorage.setItem("taskList", JSON.stringify(taskList));
+    localStorage.setItem('taskList', JSON.stringify(taskList));
 
     // renders task into DOM
     renderTask(taskList);
   };
 
   const renderTask = (taskList) => {
-    taskListDiv.innerHTML = "";
+    taskListDiv.innerHTML = '';
     taskList.forEach((task) => {
       taskListDiv.insertAdjacentHTML(
-        "afterbegin",
+        'afterbegin',
         `
         <div class="tasks">
         <p>Title:${task.title} Description:${task.description} Due Date:${task.dueDate} Priority:${task.priority}<p>
@@ -91,25 +89,25 @@ const renderHomePage = () => {
   };
 
   // renders local storage into dom
-  const taskList = JSON.parse(localStorage.getItem("taskList") || "[]");
+  const taskList = JSON.parse(localStorage.getItem('taskList') || '[]');
   renderTask(taskList);
 
   // event listeners
-  addTaskDiv.addEventListener("click", (e) => {
+  addTaskDiv.addEventListener('click', () => {
     toggleHide();
     inputText.focus();
   });
 
-  submitBtn.addEventListener("click", (e) => {
+  submitBtn.addEventListener('click', () => {
     processTaskInput();
     toggleHide();
   });
 
-  cancelBtn.addEventListener("click", (e) => {
+  cancelBtn.addEventListener('click', () => {
     toggleHide();
   });
 
-  addProjectDiv.addEventListener("click", (e) => {});
+  addProjectDiv.addEventListener('click', () => {});
 };
 
 export default renderHomePage;
